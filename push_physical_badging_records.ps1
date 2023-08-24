31e22067c978435b7ee91ed4e1cc4c038d1e8f94
Shrankhlas <141009155+Shrankhlas@users.noreply.github.com>
2023-08-24 16:37:53 +0530

    Update push_physical_badging_records.ps1

--- push_physical_badging_records.ps1
+++ push_physical_badging_records.ps1
@@ -11,7 +11,9 @@ param
     [Parameter(mandatory = $true)]
     [string] $jobId,
     [Parameter(mandatory = $true)]
-    [string] $jsonFilePath
+    [string] $jsonFilePath,
+    [Parameter(mandatory = $false)]
+    [Int] $retryTimeout = 60
 )
 
 # Access Token Config
@@ -76,10 +78,11 @@ function RetryCommand {
             }
             catch {
                 Write-Error $_.Exception.InnerException.Message -ErrorAction Continue
+                Write-Host ("Will retry in [{0}] seconds" -f $retryTimeout)
+                Start-Sleep $retryTimeout
                 if ($cnt -lt $Maximum) {
                     Write-Host "Retrying"
                 }
-                Start-Sleep 60
             }
             
         } while ($cnt -lt $Maximum)
